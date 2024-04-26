483e10bf7f820ad891caf5d535d320178ba10c82
simcat13 <rycbar0@gmail.com>
2024-04-26 10:00:08 +0900

    깃 테스트

---
+++ lucky/src/main/java/com/kh/lucky/dto/BusDto.java
@@ -0,0 +1,13 @@
+package com.kh.lucky.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class BusDto {	
+	private int butNo;
+	
+	// 테스트 생성파일
+}
