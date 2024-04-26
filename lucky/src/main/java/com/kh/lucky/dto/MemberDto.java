1b5e0b843bd8264e8c3e7fc743987bd98635c3d7
yoojun2074 <yoojun2074@naver.com>
2024-04-26 10:09:06 +0900

    업로드

---
+++ lucky/src/main/java/com/kh/lucky/dto/MemberDto.java
@@ -0,0 +1,12 @@
+package com.kh.lucky.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class MemberDto {
+	String memberId;
+
+}
