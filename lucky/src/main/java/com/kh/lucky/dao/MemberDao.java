2d8621a037fb0e628e2d195827d05537cb45c97a
simcat13 <rycbar0@gmail.com>
2024-04-29 18:06:48 +0900

    회원기본 생성

---
+++ lucky/src/main/java/com/kh/lucky/dao/MemberDao.java
@@ -0,0 +1,24 @@
+package com.kh.lucky.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.lucky.dto.MemberDto;
+
+
+@Repository
+public class MemberDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public List<MemberDto> selectList(){
+		return sqlSession.selectList("member.list");
+	}
+	public MemberDto selectOne(String memberId) {
+		return sqlSession.selectOne("member.find", memberId);
+	}
+}
--- lucky/src/main/java/com/kh/lucky/dto/MemberDto.java
+++ lucky/src/main/java/com/kh/lucky/dto/MemberDto.java
@@ -1,13 +1,35 @@
 package com.kh.lucky.dto;
 
+import java.sql.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+@ToString(exclude = {"memberPw"})
 public class MemberDto {
-	String memberId;
-	String memberPassWord;
-
+	// 4월 29일 기준 컬럼 19개
+	private String memberId; // 회원아이디
+	private String memberPw; // 회원비밀번호
+	private String memberName; // 회원이름
+	private String memberBirth; // 회원생년월일
+	private String memberSex; // 성별
+	private String memberEmail; // 회원이메일
+	private String memberPhone; // 회원연락처
+	private String memberLevel; // 회원등급(일반회원/관리자/최고관리자)
+	private Date memberJoinDate; // 가입일시
+	private Date memberLoginDate; // 로그인일시
+	private int memberPoint; // 회원포인트
+	private String memberZip; // 주소(우편번호)
+	private String memberAddr1; // 주소(기본주소)
+	private String memberAddr2; // 주소(상세주소)
+	private String providerId; // 소셜로그인 아이디
+	private String memberPrivacyAgree; // 개인정보 동의
+	private String memberPrivacyDate; // 개인정보 동의일
+	private String memberServiceAgree; // 서비스 이용약관
+	private String memberBusAgree; // 운수 이용약관
+	
 }
