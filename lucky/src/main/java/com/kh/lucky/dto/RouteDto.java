7bfd96fb21ad7a7620fea77da7dd26fa327c2209
lovejr1114 <lovejr1114@naver.com>
2024-05-01 15:29:19 +0900

    route dto, mapper 수정

--- lucky/src/main/java/com/kh/lucky/dto/RouteDto.java
+++ lucky/src/main/java/com/kh/lucky/dto/RouteDto.java
@@ -1,5 +1,7 @@
 package com.kh.lucky.dto;
 
+import java.sql.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,12 +12,11 @@ public class RouteDto {
 	private int routeNo; //노선번호
 	private String routeTime; //소요시간
 	private double routeKm; //운행거리
-	private String routeStartTime; //출발시간
-	private String routeEndTime; //도착시간
+	private Date routeStartTime; //출발시간
+	private Date routeEndTime; //도착시간
 	private String routeStart; //출발지
 	private String routeEnd; //도착지
 	private int busNo; //버스 번호
-	private String routePay; //요금
 	private String routeWay; //편도 or 왕복
 }
 
